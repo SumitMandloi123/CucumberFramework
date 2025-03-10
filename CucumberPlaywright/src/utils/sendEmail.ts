8a79855f6d4b4b3e2f23e14b9be57d9414495d56
Sumit Mandloi <sumit.mandloi@unthinkable.co>
2025-03-10 16:51:43 +0530

    Latest changes

--- CucumberPlaywright/src/utils/sendEmail.ts
+++ CucumberPlaywright/src/utils/sendEmail.ts
@@ -55,7 +55,7 @@ const mailOptions = {
 // Send the email
 transporter.sendMail(mailOptions, (err, info) => {
   if (err) {
-    console.error('❌ Error sending email:', err);
+    console.error('❌ Error while sending email:', err);
     process.exit(1);
   } else {
     console.log('✅ Email sent successfully to:', recipientList.join(', '));
